be906c71f5d4458122bd9fd15a2f40de55924e61
SerTor2 <sertor27@gmail.com>
2019-10-05 23:00:59 +0200

    camara

--- Assets/Scripts/CartController.cs
+++ Assets/Scripts/CartController.cs
@@ -24,6 +24,8 @@ public class CartController : MonoBehaviour
     public Transform ruedasDelanteras;
     public Transform ruedasTraseras;
 
+    public bool girando = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -62,6 +64,7 @@ public class CartController : MonoBehaviour
         if (horizontalDir != 0)
         {
             int direction = 0;
+            girando = true;
 
             if (horizontalDir > 0)
                 direction = 1;
@@ -71,6 +74,8 @@ public class CartController : MonoBehaviour
             float absoluteDirection = Mathf.Abs(horizontalDir);
             Rotate(direction, absoluteDirection);
         }
+        else
+            girando = false;
 
         currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f);
         speed = 0f;
