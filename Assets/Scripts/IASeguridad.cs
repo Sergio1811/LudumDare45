c4cd7867b0781d51401960f901601ecbd5a9079d
Sergio Rodriguez <pekeno1811@gmail.com>
2019-10-05 13:24:18 +0200

    Quarterback

--- Assets/Scripts/IASeguridad.cs
+++ Assets/Scripts/IASeguridad.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IASeguridad : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public enum State { Searching, Attack, Dead };
+    State m_CurrentState = State.Searching;
+    NavMeshAgent m_NMAgent;
     void Start()
     {
-        
+        m_NMAgent = this.GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        switch (m_CurrentState)
+        {
+            case State.Searching:
+
+                break;
+            case State.Attack:
+
+                break;
+            case State.Dead:
+
+                break;
+
+        }
+    }
+
+    public void ChangeState(State l_CurrentState, State l_NextState)
+    {
+        switch (l_CurrentState)
+        {
+            case State.Searching:
+                break;
+            case State.Attack:
+                break;
+            case State.Dead:
+                break;
+            default:
+                break;
+        }
+
+        switch (l_NextState)
+        {
+            case State.Searching:
+                break;
+            case State.Attack:
+                break;
+            case State.Dead:
+                break;
+            default:
+                break;
+        }
+        m_CurrentState = l_NextState;
     }
 }
