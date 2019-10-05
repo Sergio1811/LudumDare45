de5d865965ac59db5ecc3a99eced0cc08cf77f9d
Sergio Rodriguez <pekeno1811@gmail.com>
2019-10-05 04:02:53 +0200

    GameManager&AddForce

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    [HideInInspector]
+    public GameObject m_Player;
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+
+
+    }
+
+    private void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ObjectFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFall : MonoBehaviour
+{
+    public float m_ExplosionForce;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ObjectFalling();
+        }
+    }
+
+    public void ObjectFalling()
+    {
+        Rigidbody[] l_RigidBodies = GetComponentsInChildren<Rigidbody>();
+        Vector3 l_Direction = (GameManager.Instance.m_Player.transform.position - this.transform.position).normalized;
+        print(l_Direction); 
+        foreach (Rigidbody item in l_RigidBodies)
+        {
+            item.AddForce(l_Direction*m_ExplosionForce);
+        }
+    }
+}
