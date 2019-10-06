9bc8f9f21745ceaf403e67f013b90427bd1a02eb
SerTor2 <sertor27@gmail.com>
2019-10-06 14:30:35 +0200

    subir

--- Assets/Scripts/ObjetoEstanteria.cs
+++ Assets/Scripts/ObjetoEstanteria.cs
@@ -12,14 +12,35 @@ public class ObjetoEstanteria : MonoBehaviour
     float currentTime = 0;
     float maxTime;
     public Rigidbody rb;
+    public GameManager gm;
+    public List<GameObject> objetos = new List<GameObject>();
+    bool firstTime = true;
     private void Awake()
     {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            objetos.Add(transform.GetChild(i).gameObject);
+        }
         rb = gameObject.GetComponent<Rigidbody>();
         maxTime = Random.Range(20, 60);
     }
 
     private void Update()
     {
+        if(gm != null && firstTime && objetos.Count > 0)
+        {
+            firstTime = false;
+            Random.InitState(gm.random * 2);
+            gm.random++;
+            int random = Random.Range(0, objetos.Count);
+            objetos[random].SetActive(true);
+
+            foreach (GameObject go in objetos)
+            {
+                if (go != objetos[random])
+                    go.SetActive(false);
+            }
+        }
         if(caido)
         {
             currentTime += Time.deltaTime;
