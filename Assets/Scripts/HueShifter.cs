1e6121e6e6f9778ea23529394f459c9b02a824b6
Sergio Rodriguez <pekeno1811@gmail.com>
2019-10-07 17:21:41 +0200

    Menus

---
+++ Assets/Scripts/HueShifter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HueShifter : MonoBehaviour
+{
+    public float Speed = 1;
+    private Renderer rend;
+    
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+       rend.material.SetColor("_EmissiveColor", Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 0.2f)));
+    }
+}
