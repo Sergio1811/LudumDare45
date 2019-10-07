6300b545e59b693f9512769120f2911df6611019
SerTor2 <sertor27@gmail.com>
2019-10-07 15:40:24 +0200

    Merge branch 'master' of https://github.com/Sergio1811/LudumDare45

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,7 +15,6 @@ public class CameraController : MonoBehaviour
     public float minDistance = 4;
     public float maxDistance = 8;
     public float maxDistanceReal;
-    public float minDistanceReal;
 
     private float currentSpeed = 50;
     private float limitSpeed = 1.5f;
@@ -28,47 +27,62 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         maxDistanceReal = maxDistance;
-        minDistanceReal = minDistance;
-        currentSpeed = 100;
-        myCamera.transform.forward = (looker.position - gameObject.transform.position).normalized;
-        transformPositionCamera.position = transform.position;
-        goToPosition.position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CameraColliding();
+        CameraColliding();
     }
 
     private void FixedUpdate()
     {
         float distance = (player.gameObject.transform.position - myCamera.transform.position).magnitude;
-        if (!player.girando && !going && (distance <= maxDistance && distance >= minDistance))
+        if (!player.girando && !going && (distance < maxDistance && distance > minDistance))
         {
-            myCamera.transform.position = Vector3.MoveTowards(myCamera.transform.position, transformPositionCamera.position, 5 * Time.deltaTime);
+            currentSpeed = 30;
+            limitSpeed = 10f * (player.gameObject.transform.position - myCamera.transform.position).magnitude;
+            myCamera.transform.position = Vector3.MoveTowards(myCamera.transform.position, transformPositionCamera.transform.position, currentSpeed * Time.deltaTime);
         }
         else if (player.girando || distance < maxDistance || distance > minDistance)
         {
-            myCamera.transform.position -= (player.gameObject.transform.position - myCamera.transform.position).normalized * Time.deltaTime;
+            if (!going)
+                currentSpeed = 0;
+            limitSpeed = 2f * (player.gameObject.transform.position - myCamera.transform.position).magnitude;
+
             if (distance > maxDistance || distance < minDistance)
             {
                 if (distance < minDistance)
                 {
+                    currentSpeed = 20;
                     distance = minDistance;
-                    myCamera.transform.position = player.gameObject.transform.position +(myCamera.transform.position - player.gameObject.transform.position).normalized * minDistance;
-
+                    limitSpeed = 2f * (player.gameObject.transform.position - myCamera.transform.position).magnitude;
                 }
                 else if (distance > maxDistance)
                 {
                     distance = maxDistance;
-                    myCamera.transform.position = player.gameObject.transform.position + (myCamera.transform.position - player.gameObject.transform.position).normalized * maxDistance;
+                    currentSpeed = 30;
+                    limitSpeed = 10f * (player.gameObject.transform.position - myCamera.transform.position).magnitude;
+
                 }
             }
+            goToPosition.position = (transformPositionCamera.transform.position - player.gameObject.transform.position).normalized * distance + player.gameObject.transform.position; //cambiar chequeando la posición
+            going = true;
+
 
         }
+        if (going)
+        {
+            currentSpeed += acceleratePerSecond * Time.deltaTime;
+            if (currentSpeed > limitSpeed)
+                currentSpeed = limitSpeed;
+            myCamera.transform.position = Vector3.MoveTowards(myCamera.transform.position, goToPosition.transform.position, currentSpeed * Time.deltaTime);
+            if ((myCamera.transform.position - goToPosition.transform.position).magnitude < 0.05f)
+            {
+                going = false;
+            }
+        }
         myCamera.transform.forward = (looker.position - gameObject.transform.position).normalized;
-
     }
 
 
@@ -76,28 +90,18 @@ public class CameraController : MonoBehaviour
     {
         RaycastHit l_RaycastHit;
         Vector3 l_Direction = looker.transform.position - myCamera.transform.position;
-        Ray l_Ray = new Ray(goToPosition.position, l_Direction);
+        Ray l_Ray = new Ray(myCamera.transform.position, l_Direction);
         if (Physics.Raycast(l_Ray, out l_RaycastHit, layerMask))
         {
             print("Ray");
             if (l_RaycastHit.collider.gameObject.tag != "Player")
             {
-                transformPositionCamera.position = l_RaycastHit.point + l_Direction.normalized * 1f;
-                float distance = (looker.position - l_RaycastHit.point + l_Direction.normalized * 1f).magnitude;
-
-                if (distance > 2)
-                    maxDistance = distance;
-
+                myCamera.transform.position = l_RaycastHit.point + l_Direction.normalized * 1.5f;
+                goToPosition.position = l_RaycastHit.point + l_Direction.normalized * 1.5f;
+                maxDistance = (looker.position - l_RaycastHit.point + l_Direction.normalized * 1.5f).magnitude;
             }
-            else
-            {
-                minDistance = minDistanceReal;
-                maxDistance = maxDistanceReal;
-                transformPositionCamera.position = goToPosition.position;
-            }
-
-
-
+        
+               
         }
         else maxDistance = maxDistanceReal;
 
